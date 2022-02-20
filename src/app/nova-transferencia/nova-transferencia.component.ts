443a70fe8ce5b566e7903778640fe8bd3972b97a
gabrielcicero45 <gabrielcicero45@gmail.com>
2022-02-20 10:19:25 -0300

    #01 Primeiro Commit

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Transferencia } from '../models/transferencia.model';
+import { TransferenciaService } from '../services/transferencia.service';
+
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent implements OnInit {
+  @Output() aoTransferir = new EventEmitter<any>();
+  valor!: number;
+  destino!: number;
+
+  constructor(private service:TransferenciaService, private router:Router){}
+
+  transferir(){
+   console.log('Nova Transferencia');
+   const valorEmitir: Transferencia = {valor: this.valor, destino: this.destino}
+   this.service.adicionar(valorEmitir).subscribe( (resultado) =>{
+     console.log(resultado)
+     this.limparCampos()
+     this.router.navigateByUrl('extrato')
+   }, (error) => console.error(error))
+
+  }
+
+  limparCampos(){
+    this.valor = 0
+    this.destino = 0
+  }
+  ngOnInit(): void {
+  }
+
+}
